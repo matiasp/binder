156f6236d9cd50c86c5bd02a2bc9d62e01bca13e
KALMAR Alf <alf.kalmar@soprasteria.com>
2020-06-07 21:39:59 +0200

    Added summary tasks

---
+++ chapters/best_module.py
@@ -0,0 +1,6 @@
+def great_function(city):
+    if city == "Halden":
+        print("{} is the best city in Norway".format(city))
+    else:
+        print("Halden is the best city in the World")
+    
